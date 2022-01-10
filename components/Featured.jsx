47e095a2946a7b2836188204119f71f9f90d2798
Hamza Umar <humar6078@gmail.com>
2022-01-10 23:01:24 +0800

    featured done

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,24 +1,53 @@
 import styles from "../styles/Featured.module.css";
 import Image from "next/image";
+import { useState } from "react";
 
 const Featured = () => {
+  const [index, setIndex] = useState(0);
+
   const images = [
-    "/img/featured1.png",
+    "/img/featuressdd.jpg",
     "/img/featured2.png",
     "/img/featured3.png",
   ];
 
+  const handleArrow = (direction) => {
+    if (direction === "l") {
+      setIndex(index !== 0 ? index - 1 : 2);
+    } else if (direction === "r") {
+      setIndex(index !== 2 ? index + 1 : 0);
+    }
+    console.log(index);
+  };
+
   return (
     <div className={styles.container}>
-      <Image src="/img/arrowl.png" alt="" layout="fill" />
-      <div className={styles.wrapper}>
-        <div className={styles.imgContainer}>
-          {images.map((img, i) => (
-            <Image src={img} alt="" key={i} layout="fill" />
-          ))}
-        </div>
+      <div
+        className={styles.arrowContainer}
+        style={{ left: 0 }}
+        onClick={() => handleArrow("l")}
+      >
+        <Image src="/img/arrowl.png" alt="" layout="fill" />
       </div>
-      <Image src="/img/arrowr.png" alt="" layout="fill" />
+      <div
+        className={styles.wrapper}
+        style={{ transform: `translate(${-100 * index}vw)` }}
+      >
+        {images.map((img, i) => (
+          <div className={styles.imgContainer} key={i}>
+            {img}
+            <Image src={img} alt="" layout="fill" objectFit="contain" />
+          </div>
+        ))}
+      </div>
+
+      <div
+        className={styles.arrowContainer}
+        style={{ right: 0 }}
+        onClick={() => handleArrow("r")}
+      >
+        <Image src="/img/arrowr.png" alt="" layout="fill" />
+      </div>
     </div>
   );
 };
